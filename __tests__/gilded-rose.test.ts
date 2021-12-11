fa46e719786fff90e96a361a03bf33108dc1cc6b
Endika <endikaorve@gmail.com>
2021-12-11 09:38:39 +0100

    Test: Item básico: los días y calidad se degradan de 1 en 1

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -6,4 +6,16 @@ describe('Gilded Rose', function () {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual('foo');
   });
+
+  it('Item básico: los días y calidad se degradan de 1 en 1', function () {
+    const gildedRose = new GildedRose([new Item('Test básico', 10, 20)]);
+    const days = 8;
+
+    let items = [];
+    for (let index = 0; index < days; index++) {
+      items = gildedRose.updateQuality();
+    }
+
+    expect(items[0].quality).toEqual(12);
+  });
 });
