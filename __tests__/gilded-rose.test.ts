9c71cc92395601085daf7de61fdec48d68094233
Endika <endikaorve@gmail.com>
2021-12-11 09:53:05 +0100

    Test: Cualquier item: su calidad nunca debería sobrepasar 50

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -7,6 +7,12 @@ describe('Gilded Rose', function () {
     expect(items[0].name).toEqual('foo');
   });
 
+  xit('La calidad no debería ser nunca mayor a 50', function () {
+    const gildedRose = new GildedRose([new Item('foo', 10, 55)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
+
   it('Item básico: los días y calidad se degradan de 1 en 1', function () {
     const gildedRose = new GildedRose([new Item('Test básico', 10, 20)]);
     const days = 8;
@@ -67,9 +73,15 @@ describe('Gilded Rose', function () {
     expect(items[0].quality).toEqual(32);
   });
 
-  xit('La calidad no debería ser nunca mayor a 50', function () {
-    const gildedRose = new GildedRose([new Item('foo', 10, 55)]);
-    const items = gildedRose.updateQuality();
+  it('Cualquier item: su calidad nunca debería sobrepasar 50', function () {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 10, 45)]);
+    const days = 6;
+
+    let items = [];
+    for (let index = 0; index < days; index++) {
+      items = gildedRose.updateQuality();
+    }
+
     expect(items[0].quality).toEqual(50);
   });
 });
