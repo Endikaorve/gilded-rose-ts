f816ce3a9e6158b147d595981e5ac9b59aed85e6
Endika <endikaorve@gmail.com>
2021-12-11 09:38:25 +0100

    Test: El nombre no debería verse afectado

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -1,9 +1,9 @@
-import { Item, GildedRose } from "../app/gilded-rose";
+import { Item, GildedRose } from '../app/gilded-rose';
 
-describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
+describe('Gilded Rose', function () {
+  it('El nombre no debería verse afectado', function () {
+    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
+    expect(items[0].name).toEqual('foo');
   });
 });
