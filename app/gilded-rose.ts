33a93b225c4d0f9aa0e478a5d48aeb1808088138
Endika <endikaorve@gmail.com>
2021-12-11 13:04:50 +0100

    Refactor: Limpia la clase Item

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,13 +1,9 @@
 export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
-
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+  constructor(
+    public name: string,
+    public sellIn: number,
+    public quality: number
+  ) {}
 }
 
 export class GildedRose {
