ffd6f13431fea29f0fc7da566f034e35c6a7683b
Endika <endikaorve@gmail.com>
2021-12-11 09:56:22 +0100

    Test: Sulfuras: su fecha nunca cambia

--- __tests__/gilded-rose.test.ts
+++ __tests__/gilded-rose.test.ts
@@ -84,4 +84,18 @@ describe('Gilded Rose', function () {
 
     expect(items[0].quality).toEqual(50);
   });
+
+  it('Sulfuras: su fecha nunca cambia', function () {
+    const gildedRose = new GildedRose([
+      new Item('Sulfuras, Hand of Ragnaros', 10, 45),
+    ]);
+    const days = 6;
+
+    let items = [];
+    for (let index = 0; index < days; index++) {
+      items = gildedRose.updateQuality();
+    }
+
+    expect(items[0].sellIn).toEqual(10);
+  });
 });
